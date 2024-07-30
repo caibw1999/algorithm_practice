1b25b7d3e545ccede7cea6b52abe9948b51d0a0f
Andy <caibw1999@163.com>
2024-07-30 23:37:13 +0800

    feat:增加项目

---
+++ easy/src/main/java/com/caibw/array/ThirdLargestNumber.java
@@ -0,0 +1,20 @@
+package com.caibw.array;
+
+/**
+ * @ClassName ThirdLargestNumber
+ * @Package com.caibw.array
+ * @Author CaiBW
+ * @Create 24/07/30 下午 11:04
+ * @Description
+ */
+public class ThirdLargestNumber {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        
+        thirdMax(arr);
+    }
+    
+    private static void thirdMax(int[] arr) {
+    
+    }
+}
